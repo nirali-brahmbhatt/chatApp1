eea888c972b5fda77b1c9724b89f40cf93d1163a
nirali-brahmbhatt <46813160+nirali-brahmbhatt@users.noreply.github.com>
2021-12-26 00:02:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection',socket =>{
+    socket.on('new-user-joined', name1 =>{
+        console.log("New user", name1);
+        users[socket.id] = name1;
+        socket.broadcast.emit('user-joined',name1);
+    });
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive',{message:message,name1:user[socket.id]})
+    });
+});
